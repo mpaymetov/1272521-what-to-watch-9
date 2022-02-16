00ddf33b1403d0b59ae97471db30189b6eb7dcd1
Mikhail Paymetov <muxa147@gmail.com>
2022-02-16 21:15:07 +0300

    создал компонент главной страницы

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const PromoFilm = {
+  TITLE: 'The Grand Budapest Hotel',
+  GENRE: 'Drama',
+  RELEASE: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      title = {PromoFilm.TITLE}
+      genre = {PromoFilm.GENRE}
+      release = {PromoFilm.RELEASE}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
